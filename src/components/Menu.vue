9b51e4f611e94a4945746d3aebd8cc108198c4fe
Francy Rodriguez <rodriguezfrancy08@gmail.com>
2019-05-13 22:01:56 -0500

    se agrego menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,28 +3,29 @@
     <!-- <div class="headline text-xs-center pa-5">
       Active: {{ bottomNav }}
     </div> -->
-    <v-bottom-nav :value="true" absolute shift>
-      <router-link tag="li" to="/">
-        <v-btn color="teal" flat value="recent">
+    <v-bottom-nav :active.sync="activeBtn" 
+      :value="true" absolute shift>
+      <router-link tag="li" to="/Experiences">
+        <v-btn color="teal" flat value="Experiences">
           <span>Inicio</span>
           <v-icon>home</v-icon>
         </v-btn>
       </router-link>
-      <router-link tag="li" to="/profile">
-      <v-btn dark color="teal" flat value="search">
-        <span>Descubrir</span>
+      <router-link tag="li" to="/Explore">
+      <v-btn dark color="teal" flat value="Explore">
+        <span>Explorar</span>
         <v-icon>search</v-icon>
       </v-btn>
       </router-link>
-      <router-link tag="li" to="/profile">
-      <v-btn color="teal" flat value="favorite">
+      <router-link tag="li" to="/Favorite">
+      <v-btn color="teal" flat value="Favorite">
         <span>Favoritos</span>
         <v-icon>favorite</v-icon>
       </v-btn>
       </router-link>
-      <router-link tag="li" to="/profile">
-      <v-btn color="teal" flat value="person">
-        <span>Cuenta</span>
+      <router-link tag="li" to="/Profile">
+      <v-btn color="teal" flat value="Profile">
+        <span>Mi Perfil</span>
         <v-icon>person</v-icon>
       </v-btn>
       </router-link>
@@ -36,9 +37,13 @@
 export default {
   data() {
     return {
-      // bottomNav: '3'
+      activeBtn: 'Experiences'
     };
-  }
+  },
+    created() {
+    this.activeBtn = this.$route.name
+  },
+  
 };
 </script>
 
