9884f9539741912e25fd02b131face30a509cd88
Francy Rodriguez <rodriguezfrancy08@gmail.com>
2019-05-13 22:02:43 -0500

    Categorias y card de sitios

---
+++ src/components/card.vue
@@ -0,0 +1,53 @@
+<template>
+    <!-- <v-layout>
+        <v-flex xs6 sm6 offset-sm3> -->
+            <div class="content-card">
+            <v-card >
+                <v-img
+                    src="https://images.pexels.com/photos/2034335/pexels-photo-2034335.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    height="100px"
+                ></v-img>
+                <v-card-title >
+                    <div class="title">Nombre del sitio</div>
+                </v-card-title>
+                <v-card-text>
+                    <v-rating 
+                        v-model="rating"
+                        color="yellow darken-3"
+                        background-color="grey darken-1"
+                        empty-icon="$vuetify.icons.ratingFull"
+                        half-increments
+                        hover
+                        size="18"
+                        dense
+                        ></v-rating>
+                </v-card-text>
+            </v-card>
+            </div>
+        <!-- </v-flex>
+    </v-layout> -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.v-card__text {
+  padding-top: 0;
+  text-align: left;
+}
+.v-card__title {
+  padding-bottom: 0;
+  text-align: left;
+}
+.content-card {
+    /* box-sizing: border-box;
+    padding: 5px;
+    max-width: 200px;
+    width: 100%; */
+}
+.title {
+    font-size: 14px !important;
+}
+</style>
---
+++ src/components/categorys.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="container-circles">
+    <div class="circle orange">
+        <v-icon>restaurant</v-icon>
+    </div>
+    <div class="circle gold" >
+        <div prepend-icon="person_outline"></div>
+        <v-icon>account_balance</v-icon>
+    </div>
+    <div class="circle clrGreen">
+        <v-icon>terrain</v-icon>
+    </div>
+    <div class="circle clrBlue">
+        <v-icon>hotel</v-icon>
+    </div>
+    <div class="circle clrPurple">
+        <v-icon>local_bar</v-icon>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.circle {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    /* background-color: blueviolet; */
+    margin: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+}
+.container-circles {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+}
+.gold{
+    background-color: #ffc107;
+}
+.clrBlue{
+    background-color: #03a9f4;
+}
+.clrPurple{
+    background-color: #ab47bc;
+}
+.clrGreen{
+    background-color: #4caf50;
+}
+</style>
