f99e60ae19c204f8ea2af0732f7cc873107338f4
Francy Rodriguez <rodriguezfrancy08@gmail.com>
2019-05-20 20:53:52 -0500

    Btn flotante, ajustes de profileInformation y post

---
+++ src/components/ProfileInformation.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="profileInfo">
+        <div class="content-avatar text-xs-left">
+            <div class="display-1">Pedro Perez</div>
+            <div><span><b>120 </b>Seguidores </span> <span><b>120 </b>Siguiendo</span></div>
+            <v-btn outline color="blue-grey" class="ma-0 my-2" round small>Editar perfil</v-btn>
+        </div>
+            <v-avatar class="avatar" size="80px">
+                <img src="https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="John" >
+            </v-avatar>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.content-avatar{
+    /* margin: 16px;s */
+}
+.profileInfo {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-left: 20px;
+    border-bottom: 1px solid #eee;
+    padding-bottom: 15px;
+    margin-bottom: 5px;
+}
+.avatar {
+    margin: 0 20px 0 20px;
+}
+.email {
+    letter-spacing: .5px !important;
+}
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="profile">
-        <v-toolbar dark color="teal">
+        <v-toolbar dark fixed color="teal">
             <v-toolbar-title class="white--text">Mi perfil</v-toolbar-title>
             <v-spacer></v-spacer>
         </v-toolbar>
-        <Followers />
-        <Post />
+        <ProfileInformation />
+        <!-- <Followers /> -->
+        <Post v-for="post in 4" />
         <Menu />
+            <v-btn
+              fixed
+              dark
+              fab
+              bottom=""
+              right
+              color="orange"
+              class="btn-float"
+            >
+              <v-icon>add</v-icon>
+            </v-btn>
     </div>
 </template>
 
@@ -14,16 +26,25 @@
 import Menu from './../components/Menu'
 import Followers from './../components/Followers'
 import Post from './../components/Post'
+import ProfileInformation from './../components/ProfileInformation'
 
 export default {
     components: {
         Menu,
         Followers,
-        Post
+        Post,
+        ProfileInformation
     }
 }
 </script>
 
 <style scoped>
-
+.profile {
+    margin-top: 80px;
+    margin-bottom: 60px;
+    position: relative;
+}
+.btn-float {
+    bottom: 70px;
+}
 </style>
