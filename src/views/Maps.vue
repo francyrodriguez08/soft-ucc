b05cada32654e0b5a2f2d551da72de2f2798fa48
Francy Rodriguez <rodriguezfrancy08@gmail.com>
2019-05-30 19:06:08 -0500

    Se agregaron los puntos de en el mapa

--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -5,15 +5,26 @@
           <v-toolbar-title class="white--text">Experiencias</v-toolbar-title>
           <v-spacer></v-spacer>
       </v-toolbar>
-      <v-toolbar dense>
-        <!-- <div class="inputPlace"> -->
-          <v-text-field placeholder="¿A donde quieres ir?" prepend-inner-icon="search" color="teal" ></v-text-field>
-          <v-icon @click="redirect('/experiences')">send</v-icon>
-        <!-- </div> -->
-      </v-toolbar>
+      <div class="container">
+        <div class="inputPlace">
+          <v-text-field placeholder="¿A donde quieres ir?" single-line prepend-inner-icon="search" color="teal" v-model="search" ></v-text-field>
+          <v-icon @click="searchText()">send</v-icon>
+        </div>
       <Categorys />
+      </div>
       <gmap-map  ref="myMap" :center="center" :zoom="15" style="width: 100%; height: 600px" >
-          <gmap-marker v-for="(item, index) in markers" :key="index" :position="getPosition(item)"/>
+          <gmap-marker v-for="(item, index) in markers" :key="index" 
+            :position="getPosition(item)"
+            @click="openInfoWindow(item,index)"
+          />
+          <gmap-info-window
+            :options="infoOptions"
+            :position="infoWindowPos"
+            :opened="infoWinOpen"
+            @closeclick="infoWinOpen=false"
+          >
+            <div v-html="infoContent"></div>
+          </gmap-info-window>
       </gmap-map>
       
     </div>
@@ -38,7 +49,20 @@ export default {
         }
       ],
       places: [],
-      currentPlace: null
+      currentPlace: null,
+      search: "",
+      infoContent: "",
+      infoWinOpen: false,
+      infoWindowPos: {
+        lat: 0,
+        lng: 0
+      },
+      infoOptions: {
+        pixelOffset: {
+          width: 0,
+          height: -35
+        }
+      }
     };
   },
   computed: {
@@ -53,95 +77,92 @@ export default {
       this.searchCategory(value);
     }
   },
-    mounted() {
-      this.geolocate();
-    },
-    methods: {
-      searchCategory(text) {
-        const villavo = new google.maps.LatLng(4.133999, -73.625197);
-        const request = {
-          location: villavo,
-          query: text,
-          fields: ["name", "geometry"]
-        };
-        const service = new google.maps.places.PlacesService(
-          this.$refs.myMap.$mapObject
-        );
-        service.textSearch(request, function(results, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-              console.log(results[i]);
-            }
-          }
-        });
-      },
-      setPlace(place) {
-        this.currentPlance = place;
-      },
-      getPosition(marker) {
-        return {
-          lat: parseFloat(marker.lat),
-          lng: parseFloat(marker.lng)
-        };
-      },
-      addMarker() {
-        const marker = {
-          nombre: "Parque de la vida",
-          lat: 4.1466671,
-          lng: -73.6225355
-        };
-        this.markers.push(marker);
-        this.center = marker;
-      },
-      geolocate: function() {
-        navigator.geolocation.getCurrentPosition(
-          position => {
-            this.center = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-          },
-          error => {
-            console.log("Error>", error);
-          }
-        );
-      },
-      redirect(ruta) {
-        this.$router.push(ruta);
+  mounted() {
+    this.geolocate();
+  },
+  methods: {
+    openInfoWindow: function(marker, idx) {
+      console.log(marker);
+      this.infoWindowPos = {
+        lat: marker.lat,
+        lng: marker.lng
+      };
+      let photoUrl = "";
+      if (marker.details.photos.length > 0) {
+        photoUrl = marker.details.photos[0].getUrl();
       }
+      this.infoContent = `<div>
+                              <div>${marker.nombre}</div>
+                              <img width='200' src='${photoUrl}' />
+                            </div>`;
+      this.infoWinOpen = true;
+    },
+    searchText() {
+      this.searchCategory(this.search);
+    },
+    searchCategory(text) {
+      const villavo = new google.maps.LatLng(4.133999, -73.625197);
+      const request = {
+        location: villavo,
+        query: text,
+        fields: ["name", "geometry"]
+      };
+      const service = new google.maps.places.PlacesService(
+        this.$refs.myMap.$mapObject
+      );
+      let _this = this;
+      service.textSearch(request, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            _this.addMarker(results[i]);
+          }
+        }
+      });
+    },
+    setPlace(place) {
+      this.currentPlance = place;
+    },
+    getPosition(marker) {
+      return {
+        lat: parseFloat(marker.lat),
+        lng: parseFloat(marker.lng)
+      };
+    },
+    addMarker(item) {
+      const marker = {
+        nombre: item.name,
+        lat: item.geometry.location.lat(),
+        lng: item.geometry.location.lng(),
+        details: item
+      };
+      this.markers.push(marker);
+      this.center = marker;
+    },
+    geolocate: function() {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+        },
+        error => {
+          console.log("Error>", error);
+        }
+      );
+    },
+    redirect(ruta) {
+      this.$router.push(ruta);
     }
   }
-
+};
 </script>
 
 <style scoped>
-.circle {
-  width: 60px;
-  height: 60px;
-  border-radius: 50%;
-  /* background-color: blueviolet; */
-  margin: 5px;
+.inputPlace{
   display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
 }
-.container-circles {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 15px;
-}
-.gold {
-  background-color: #ffc107;
-}
-.clrBlue {
-  background-color: #03a9f4;
-}
-.clrPurple {
-  background-color: #ab47bc;
-}
-.clrGreen {
-  background-color: #4caf50;
+.container{
+  padding: 0 15px;
 }
 </style>
